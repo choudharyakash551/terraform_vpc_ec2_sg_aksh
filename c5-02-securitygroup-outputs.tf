e0f6d348950f37571f6ef85b04aff6b2997edcd0
akash choudhary <choudharyakash551@gmail.com>
2024-01-29 20:01:18 +0530

    version update

--- c5-02-securitygroup-outputs.tf
+++ c5-02-securitygroup-outputs.tf
@@ -1,16 +1,16 @@
 output "public_bastion_sg_group_id" {
   description = "The ID of the security group"
-  value       = module.security-group-bestion.public_bastion_sg_group_id
+  value       = module.security-group-bestion.security_group_id
 }
 
 output "public_bastion_sg_group_vpc_id" {
   description = "The VPC ID"
-  value       = module.security-group-bestion.public_bastion_sg_group_vpc_id
-}
+  value       = module.security-group-bestion.security_group_vpc_id
+  }
 
 output "public_bastion_sg_group_name" {
   description = "The name of the security group"
-  value       = module.security-group-bestion.public_bastion_sg_group_name
+  value       = module.security-group-bestion.security_group_name
 }
 
 
@@ -18,15 +18,15 @@ output "public_bastion_sg_group_name" {
 
 output "private_sg_group_id" {
   description = "The ID of the security group"
-  value       = module.security-group-private.public_bastion_sg_group_id
+  value       = module.security-group-private.security_group_id
 }
 
 output "private_bastion_sg_group_vpc_id" {
   description = "The VPC ID"
-  value       = module.security-group-private.private_bastion_sg_group_vpc_id
+  value       = module.security-group-private.security_group_vpc_id
 }
 
 output "private_bastion_sg_group_name" {
   description = "The name of the security group"
-  value       = module.security-group-private.private_bastion_sg_group_name
+  value       = module.security-group-private.security_group_name
 }
